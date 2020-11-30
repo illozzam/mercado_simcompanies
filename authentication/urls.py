c1e31d3e1c6a8ac5c8ffcbabbab616c03c44b38d
Ricardo Lima Mazzolli <ricarm@gmail.com>
2020-11-30 11:38:23 -0300

    Cadastro de usuário melhorado e mecanismos de edição de dados de usuário

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, logout_then_login
-from .views import UserCreateView
+from .views import UserCreateView, UserUpdateView, UserPasswordChangeView
 
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='user-create'),
-    
+    path('user-update/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
+    path('user-update/password/', UserPasswordChangeView.as_view(), name='password_change'),
+
     path('login/', LoginView.as_view(template_name='authentication/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', logout_then_login, name='logout'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
-from .forms import UserCreationForm
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib import messages
+from main.models import Log
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
 
 class UserCreateView(CreateView):
     form_class = UserCreationForm
@@ -9,4 +14,38 @@ class UserCreateView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
+        messages.success(self.request, 'Usuário cadastrado com sucesso. Faça login para continuar.')
+        Log.objects.create(
+            user=user,
+            description='Cadastro de usuário.'
+        )
         return redirect(self.success_url)
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = UserChangeForm
+    template_name = 'authentication/user_update.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, 'Dados atualizados.')
+        Log.objects.create(
+            user=user,
+            description='Usuário editou seus dados.'
+        )
+        return redirect(self.success_url)
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'authentication/password_change.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        Log.objects.create(
+            user=self.request.user,
+            description='Alterou a senha.'
+        )
+        messages.success(self.request, 'Senha alterada com sucesso.')
+        return super().form_valid(form)
